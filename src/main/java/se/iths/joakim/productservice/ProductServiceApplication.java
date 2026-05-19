7b09bfc26099fb22568d49f733d3b7cf82124463
Joajak92 <Joakim.jakobsson@iths.segit config --global user.email Joakim.jakobsson@iths.se--global user.name Joajak92--global user.email Joakim.jakobsson@iths.se>
2026-05-19 10:43:38 +0200

    Initial commit

---
+++ src/main/java/se/iths/joakim/productservice/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package se.iths.joakim.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+
+}

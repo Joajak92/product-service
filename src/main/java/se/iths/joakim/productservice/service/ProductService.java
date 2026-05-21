16a9229596474564f248114f8f812b55b51026eb
Joajak92 <Joakim.jakobsson@iths.segit config --global user.email Joakim.jakobsson@iths.se--global user.name Joajak92--global user.email Joakim.jakobsson@iths.se>
2026-05-21 15:38:19 +0200

    Kommenterade ut transactional service så länge

--- src/main/java/se/iths/joakim/productservice/service/ProductService.java
+++ src/main/java/se/iths/joakim/productservice/service/ProductService.java
@@ -1,6 +1,5 @@
 package se.iths.joakim.productservice.service;
 
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import se.iths.joakim.productservice.dto.ProductRequestDto;
@@ -47,13 +46,13 @@ public class ProductService {
         productRepository.delete(product);
     }
 
-    @Transactional
-    public List<ProductResponseDto> decreaseStock(
-            List<ProductStockRequest> requestedProducts) {
-// hämta alla produkter
-// kontrollera att varje produkt verkligen finns och att stock räcker
-// kasta exception om inte en produkt finns eller stock inte räcker
-// minska stock
-// returnera produktinfo
-    }
+//    @Transactional
+//    public List<ProductResponseDto> decreaseStock(
+//            List<ProductStockRequest> requestedProducts) {
+//// hämta alla produkter
+//// kontrollera att varje produkt verkligen finns och att stock räcker
+//// kasta exception om inte en produkt finns eller stock inte räcker
+//// minska stock
+//// returnera produktinfo
+//    }
 }

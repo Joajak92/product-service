5c8c1551ded47c1721745f63125130ca33e3bf6b
Joajak92 <Joakim.jakobsson@iths.segit config --global user.email Joakim.jakobsson@iths.se--global user.name Joajak92--global user.email Joakim.jakobsson@iths.se>
2026-05-26 15:14:40 +0200

    Lade till permissions för stock decrease i SecurityConfig

--- src/main/java/se/iths/joakim/productservice/config/SecurityConfig.java
+++ src/main/java/se/iths/joakim/productservice/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
         http.authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.GET, "/products/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers(HttpMethod.POST, "/products").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/products/stock/decrease").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/products/**").hasRole("ADMIN")
                         .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()

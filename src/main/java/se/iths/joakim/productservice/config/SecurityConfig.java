fe776f66c0a68e2dbca6c315e2376dffe843518b
Joajak92 <Joakim.jakobsson@iths.segit config --global user.email Joakim.jakobsson@iths.se--global user.name Joajak92--global user.email Joakim.jakobsson@iths.se>
2026-05-26 14:02:50 +0200

    Ändring i SecurityConfig

--- src/main/java/se/iths/joakim/productservice/config/SecurityConfig.java
+++ src/main/java/se/iths/joakim/productservice/config/SecurityConfig.java
@@ -40,9 +40,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-        return http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorize -> authorize
+        http.authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.GET, "/products/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers(HttpMethod.POST, "/products").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/products/**").hasRole("ADMIN")
@@ -52,7 +50,8 @@ public class SecurityConfig {
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
                 )
-                .build();
+                .csrf(csrf -> csrf.disable());
+        return http.build();
     }
 
     @Bean
